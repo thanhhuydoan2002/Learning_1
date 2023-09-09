5559312753f87e7d8c71fa8d3dd991cf33a31819
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-09-09 10:31:33 +0700

    basic syntax

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,5 +1,12 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
+
+  dart_application_1.basicSyntax();
+
+  var n = 99;
+
+  dart_application_1.conditionalExpression(n);
 }
--- lib/dart_application_1.dart
+++ lib/dart_application_1.dart
@@ -1,3 +1,57 @@
 int calculate() {
   return 6 * 7;
 }
+
+void basicSyntax() {
+  //Starting Point
+  print('Hello world!');
+
+  //Variables
+  int year = 2023;
+  print('int year: $year, dataType: ${year.runtimeType}');
+
+  var month = 01;
+  print('var month: $month, dataType: ${month.runtimeType}');
+
+  double day; //null
+
+  //DYNAMIC VALUE
+  dynamic a;
+  print('dynamic a: $a, dataType: ${a.runtimeType}');
+  a = 14;
+  print('dynamic a: $a, dataType: ${a.runtimeType}');
+  a = 15.6;
+  print('dynamic a: $a, dataType: ${a.runtimeType}');
+  a = 'text';
+  print('dynamic a: $a, dataType: ${a.runtimeType}');
+  a = true;
+  print('dynamic a: $a, dataType: ${a.runtimeType}');
+
+  const hourInDay = 24;
+  // hourInDay = 22; ERROR
+
+  final age = 23;
+  // age = 33; ERROR
+}
+
+void conditionalExpression(int n) {
+  //IF ELSE
+  if (n == 1)
+    print('so mot');
+  else if (n == 99)
+    print('so may man');
+  else
+    print('so khac');
+
+  //SWITCH CASE
+  switch (n) {
+    case 1:
+      print('number one');
+      break;
+    case 99:
+      print('lucky number');
+      break;
+    default:
+      print('other number');
+  }
+}
