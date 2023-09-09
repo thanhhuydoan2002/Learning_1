8eea4795fd4533a1341302e03fe7d13b8940d924
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-09-09 12:54:31 +0700

    basic syntax

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -3,17 +3,20 @@ import 'package:dart_application_1/conditionalExpression.dart';
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 import 'package:dart_application_1/loop.dart';
+import 'package:dart_application_1/stringInterpolation.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+  // print('Hello world: ${dart_application_1.calculate()}!');
 
-  basicSyntax();
+  // basicSyntax();
 
-  var n = 15;
+  // var n = 15;
 
-  conditionalExpression(n);
+  // conditionalExpression(n);
 
-  final list = ['a', 'b', 'c', 'd', 'e'];
+  // final list = ['a', 'b', 'c', 'd', 'e'];
 
-  loop(list);
+  // loop(list);
+
+  stringInterpolation();
 }
---
+++ lib/stringInterpolation.dart
@@ -0,0 +1,12 @@
+void stringInterpolation() {
+  int x = 6;
+  int y = 9;
+
+  print(x + y); //15
+
+  var name = 'flutter';
+
+  print(name.toUpperCase()); //FLUTTER
+
+  print('$x$y'); //69
+}
